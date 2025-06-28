7cf539fae32b640da41a23605fa0545ab61f2a37
KMobin555 <kamrujjamanmobin123@gmail.com>
2025-06-28 11:08:20 +0600

    text processor

--- text_analyzer.py
+++ text_analyzer.py
@@ -4,6 +4,8 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+import string
+from .constants import STOPWORDS
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -15,6 +17,27 @@ class TextAnalyzer:
         self.tfidf = use_tfidf
         self.lemmatizer = WordNetLemmatizer() if use_nltk else None
         self.nltk_stoopwords = set(stopwords.words('english')) if use_nltk else None
+
+    def _preprocess_text(self, text: str) -> list[str]:
+        translator = str.maketrans('', '', string.punctuation)
+        text = text.translate(translator).lower()
+
+        if self.use_nltk:
+            words = word_tokenize(text)
+            words = [
+                self.lemmatizer.lemmatize(word)
+                for word in words
+                if word not in self.nltk_stoopwords and len(word) > 2
+            ]
+        else:
+            words = text.split()
+            words = [
+                word for word in words
+                if word not in STOPWORDS and len(word) > 2
+            ]
+
+        return words
+
 
     @staticmethod
     def parse_messages(chat_lines: List[str]) -> Dict[str,list[str]]:
